eb6191aa52199fd978c596e59dc46d0b2dc18b95
Степан <@LAPTOP-B0PF4KGO>
2023-08-29 15:43:32 +0500

    .

--- Assets/Scripts/UIClasses/Popups/PopupLogInPanel.cs
+++ Assets/Scripts/UIClasses/Popups/PopupLogInPanel.cs
@@ -1,18 +1,22 @@
-﻿using Assets.Scripts.UIClasses.Menus;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 namespace Assets.Scripts.UIClasses.Popups
 {
-    public class PopupLogInPanel : MonoBehaviour
+    public class LogInPopupPanel : MonoBehaviour
     {
         [SerializeField] private Button loginButton;
         [SerializeField] private TMP_InputField login;
         [SerializeField] private TMP_InputField password;
         [SerializeField] private ErrorLoginPanel errorPanel;
-        [SerializeField] private MenuAdminPanel menuAdminPanel;
-        [SerializeField] private MenuUserPanel menuUserPanel;
 
         private void Awake()
         {
@@ -21,11 +25,9 @@ namespace Assets.Scripts.UIClasses.Popups
 
         private void Enter()
         {
-            if (login.text == "AAA" && password.text == "123")
+            if(login.text == "AAA" && password.text == "123")
             {
-                menuUserPanel.gameObject.SetActive(false);
-                menuAdminPanel.gameObject.SetActive(true);
-                gameObject.SetActive(false);
+
             }
             else
             {
