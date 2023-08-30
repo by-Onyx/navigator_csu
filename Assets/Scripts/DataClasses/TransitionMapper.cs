dd9a0d71f6074a77a0459ea619a46419f6225723
Степан <@LAPTOP-B0PF4KGO>
2023-08-30 23:43:59 +0500

    added last mapper

---
+++ Assets/Scripts/DataClasses/TransitionMapper.cs
@@ -0,0 +1,19 @@
+﻿using Assets.Scripts.DataClasses.Properties.MapItemProperties;
+using DataClasses.Models;
+
+namespace DataClasses
+{
+    public static class TransitionMapper
+    {
+        public static TransitionProperties Map(TransitionModel model)
+        {
+            return new TransitionProperties
+            {
+                X = model.x,
+                Y = model.y,
+                Id = model.id,
+                TransitionClass = model.transitionType.id
+            };
+        }
+    }
+}
